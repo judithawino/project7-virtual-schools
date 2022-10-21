59bcb5190b9c736c77711e1ffbeb14dc2ea31af5
glenn-nguyo <glennnguyo1000@gmail.com>
2022-10-21 23:54:19 +0300

    student

--- db/migrate/20221021164851_create_students.rb
+++ db/migrate/20221021164851_create_students.rb
@@ -1,12 +1,12 @@
 class CreateStudents < ActiveRecord::Migration[7.0]
   def change
     create_table :students do |t|
-      t.string :id
+      t.integer :id
       t.string :name
       t.string :email
       t.string :password
-      t.string :school_id
-      t.string :course_id
+      t.integer :school_id
+      t.integer :course_id
 
       t.timestamps
     end
