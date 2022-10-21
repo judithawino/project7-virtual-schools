2b392a6e0c96bd1a90551f693ff7e9e10c7d6170
Judith Awino Omondi <judith.awino.omondi@gmail.com>
2022-10-21 20:26:21 +0300

    Migrated and seeded data

--- db/migrate/20221021161457_create_exams.rb
+++ db/migrate/20221021161457_create_exams.rb
@@ -2,7 +2,8 @@ class CreateExams < ActiveRecord::Migration[7.0]
   def change
     create_table :exams do |t|
       t.string :title
-      t.datetime :start_time
+      t.date :exam_date
+      t.time :start_time
       t.integer :duration
       t.integer :course_id
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "Seeding data"
+
+Exam.create(title: "Exam", exam_date: "3/4/2022", start_time: "8:30 a.m", duration: 60, course_id: 3)
+Exam.create(title: "Quiz", exam_date: "3/4/2022", start_time: "10:30 a.m", duration: 45, course_id: 3)
+Exam.create(title: "Assessment", exam_date: "3/4/2022", start_time: "12:30 a.m", duration: 60, course_id: 3)
+puts "completed Seeding data"
