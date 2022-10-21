01bdb6332ccce2ec3b7004f8fcd552f176284e64
Judith Awino Omondi <judith.awino.omondi@gmail.com>
2022-10-21 20:42:33 +0300

    created exams#show

--- app/controllers/exams_controller.rb
+++ app/controllers/exams_controller.rb
@@ -1,8 +1,22 @@
 class ExamsController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :exam_not_found
 
     # GET "/"
     def index
         exams = Exam.all
         render json: exams, status: :ok
+    end
+
+    def show
+        exams = find_exam
+        render json: exams, status: :ok
+    end
+
+    private
+    def find_exam
+        Exam.find(params[:id])
+    end
+    def exam_not_found
+        render json: {"error": "Not found"}, status: :not_found
     end
 end
