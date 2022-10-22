f857a00eaaa1e05c8638d93be6bb0e6267e2a983
github_Jeptooleonida <leonida.jeptoo@student.moringaschool.comgit config --global user.name Jeptooleonidagit config --global user.email leonida.jeptoo@student.moringaschool.com>
2022-10-22 19:43:40 +0300

    add the actions to the controller

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,2 +1,51 @@
 class CoursesController < ApplicationController
+
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+   
+
+    def index
+        courses = Course.all
+        render json: courses
+    end
+
+    def show
+        course = find_course
+        render json: course
+    end
+
+       
+    def create 
+        course = Course.create!(educator_params)
+        render json: course
+    end
+
+    def update
+        course = find_course
+        course.update!(course_params)
+        render json: course
+    end
+
+    def destroy
+        course = find_course
+        course.destroy
+        head :no_content
+    end
+
+    private 
+    def find_course
+        Course.find(params[:id])
+    end
+
+    def course_params
+        params.permit(:title, :duration, :image_url)
+    end
+
+    def render_not_found_response
+        render json: { error: "Course not found" }, status: :not_found
+    end
+
+    def render_unprocessable_entity_response(invalid)
+        render json: { errors: invalid.record.errors }, status: :unprocessable_entity
+    end
 end
