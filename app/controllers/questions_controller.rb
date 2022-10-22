528604a685e221a82b3781e59a98830c1a2e6e40
Judith Awino Omondi <judith.awino.omondi@gmail.com>
2022-10-22 08:57:19 +0300

    Created questions#show

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,8 +1,24 @@
 class QuestionsController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :question_not_found
 
     # GET "/questions"
     def index
         questions = Question.all
         render json: questions, status: :ok
+    end
+
+    # GET "/questions/:id"
+    def show
+        questions = find_question
+        render json: questions, status: :ok
+    end
+
+
+    private
+    def find_question
+        Question.find(params[:id])
+    end
+    def question_not_found
+        render json: {"error": "Not found"}, status: :not_found
     end
 end
