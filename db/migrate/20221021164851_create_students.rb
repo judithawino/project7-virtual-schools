a4d492f05c325632c86dcce1fe091cc0a5c55714
github_Jeptooleonida <leonida.jeptoo@student.moringaschool.comgit config --global user.name Jeptooleonidagit config --global user.email leonida.jeptoo@student.moringaschool.com>
2022-10-22 19:12:10 +0300

    update the fields

--- db/migrate/20221021164851_create_students.rb
+++ db/migrate/20221021164851_create_students.rb
@@ -1,7 +1,6 @@
 class CreateStudents < ActiveRecord::Migration[7.0]
   def change
     create_table :students do |t|
-      t.integer :id
       t.string :name
       t.string :email
       t.string :password
