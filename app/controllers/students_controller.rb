93dc70b6dfb21f11c35005f0cc9361704f0bc362
Judith Awino Omondi <judith.awino.omondi@gmail.com>
2022-10-31 05:30:35 +0300

    Added skip before action

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,4 +1,5 @@
 class StudentsController < ApplicationController
+    skip_before_action :authorize
     rescue_from ActiveRecord::RecordNotFound, with: :render_student_not_found_response
     rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
   
