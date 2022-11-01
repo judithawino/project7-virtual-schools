baad647ff5d070851c843d3ac167c123937e1a30
Judith Awino Omondi <judith.awino.omondi@gmail.com>
2022-11-01 13:48:59 +0300

    Modified

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,5 @@
 class Question < ApplicationRecord
     belongs_to :exam
-    has_one :answer, dependent: :destro
+    has_one :answer, dependent: :destroy
     validates :question, presence: true
 end
